8ab9bfb484f3021996160d34ad0293b1fecf4786
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-03-21 15:54:57 -0400

    update /app

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const {
+		children
+	}: {
+		children: Snippet<[]>;
+	} = $props();
+</script>
+
+<div>
+	{@render children?.()}
+</div>
