b97f9acf818ac6821570bc450779f358ecb60d5b
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-03-21 14:00:17 -0400

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import '@fontsource-variable/roboto';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
+<style lang="scss">
+	:global(*) {
+		font-family: 'Roboto Variable', sans-serif;
+	}
+</style>
